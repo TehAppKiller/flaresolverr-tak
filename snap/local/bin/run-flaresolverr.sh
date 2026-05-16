e5b9c5b9be330376ebf9f0840d7eb7cdadc4816c
TehAppKiller <47398236+TehAppKiller@users.noreply.github.com>
2026-05-16 10:34:21 +0200

    Always copy Chromedriver
    +Fix: Always copy Chromedriver to avoid error with snap updates

--- snap/local/bin/run-flaresolverr.sh
+++ snap/local/bin/run-flaresolverr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 # Fix Chromedriver detection (avoid undetected-chromedriver_x64 to DL different version, especially for none-x64 arch)
-if [ ! -f $SNAP_USER_DATA/.local/share/chromedriver ]; then
+#if [ ! -f $SNAP_USER_DATA/.local/share/chromedriver ]; then
 	echo "Fix ChromeDriver detection..."
     cp $SNAP/usr/lib/chromium-browser/chromedriver $SNAP_USER_DATA/.local/share/chromedriver
-fi
+#fi
 
 cd $SNAP/Flaresolverr
 python3 flaresolverr.py
